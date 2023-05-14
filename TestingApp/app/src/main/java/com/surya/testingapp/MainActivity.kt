86bb67cdb44fa434b96bb1680a21df5fe0cd0c46
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-14 20:54:29 +0700

    Test Eror NPE

--- TestingApp/app/src/main/java/com/surya/testingapp/MainActivity.kt
+++ TestingApp/app/src/main/java/com/surya/testingapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.surya.testingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var btnSetValue: Button? = null
+    private lateinit var tvText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_set_value) {
+            tvText.text ="19"
+        }
     }
 }
