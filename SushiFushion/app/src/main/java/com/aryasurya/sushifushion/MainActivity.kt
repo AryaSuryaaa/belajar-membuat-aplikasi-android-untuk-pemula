24c97910450beef7a236f6f00d7d26181a66f5b8
AryaSuryaa <suryasaputra914@gmail.com>
2023-08-30 12:25:19 +0700

    add menu_layout.xml to activity_main.xml and atur fixed size RecyclerView at MainActivity.kt

--- SushiFushion/app/src/main/java/com/aryasurya/sushifushion/MainActivity.kt
+++ SushiFushion/app/src/main/java/com/aryasurya/sushifushion/MainActivity.kt
@@ -2,10 +2,17 @@ package com.aryasurya.sushifushion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvSushis: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvSushis = findViewById(R.id.rv_sushis)
+        rvSushis.setHasFixedSize(true)
+
     }
 }
