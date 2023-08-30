ad4371274e729be7bd5bf9f2c2d9ff82896e94a4
AryaSuryaa <suryasaputra914@gmail.com>
2023-08-30 12:09:26 +0700

    Add plugin parcelize and implement to Sushis.kt

--- SushiFushion/app/src/main/java/com/aryasurya/sushifushion/Sushis.kt
+++ SushiFushion/app/src/main/java/com/aryasurya/sushifushion/Sushis.kt
@@ -1,7 +1,11 @@
 package com.aryasurya.sushifushion
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Sushis(
     val nam: String,
     val price: String,
     val photo: Int
-)
+) : Parcelable
