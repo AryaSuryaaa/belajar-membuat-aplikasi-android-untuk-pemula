f587a92ed26d0dbf240ecb8648160f336f6dfceb
AryaSuryaa <suryasaputra914@gmail.com>
2023-09-01 09:22:25 +0700

    SplashScreen

--- SushiFushion/app/src/main/java/com/aryasurya/sushifushion/SplashActivity.kt
+++ SushiFushion/app/src/main/java/com/aryasurya/sushifushion/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.aryasurya.sushifushion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Menunda tampilan activity utama dengan handler
+        Handler().postDelayed({
+            // Intentkan atau pindahkan ke activity utama setelah jangka waktu tertentu
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup activity splash screen agar tidak dapat kembali ke sana
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
